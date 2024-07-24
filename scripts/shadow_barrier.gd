d82a5e73eda02ef3e65d9db416c6660a42ea7190
Samuel Murray <samuela.y.murray@gmail.com>
2024-07-24 19:58:18 -0400

    adds hiddenParent bool to shadow barrier

--- scripts/shadow_barrier.gd
+++ scripts/shadow_barrier.gd
@@ -1,7 +1,7 @@
 extends StaticBody2D
 
 var _parent: Node2D
-
+@export var hiddenParent: bool = false;
 @onready var light_detection = $"Light Detection"
 @onready var collision_shape_2d = $CollisionShape2D
 # Called when the node enters the scene tree for the first time.
@@ -13,5 +13,5 @@ func _process(_delta):
 	# Needs timer to keep disable for set time. You will see what I mean while testing
 	# Removes collision and reveals parent e.g. bridge
 	collision_shape_2d.disabled = !light_detection.is_lit
-	if _parent:
-		_parent.visible = light_detection.is_lit
+	if hiddenParent:
+		_parent.visible = !light_detection.is_lit
